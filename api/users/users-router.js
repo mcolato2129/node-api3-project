e71bd8818f4269e6f66f56b8196ea83505b63b23
Marlon Colato <mcolato2104@gmail.com>
2024-01-16 18:55:54 -0500

    j

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -34,12 +34,15 @@ router.post('/', validateUser, (req, res, next) => {
   .catch(next);
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user)
-  console.log(req.name)
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+    Users.update(req.params.id, { name: req.name })
+    .then(() => {
+      return Users.getById(req.params.id)
+      .then(updatedUser => {
+        res.json(updatedUser)
+      })
+      .catch(next)
+    })
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
